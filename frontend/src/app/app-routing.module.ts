daf99dd8846c0e9d00f61b307d8ab94a7a9d732b
Devjish A <devjish.a@zohocorp.com>
2019-02-10 18:58:08 +0530

    Initial Routing Pages.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import {NgModule} from '@angular/core';
+import { WeighingPageComponent } from './weighing-page/weighing-page.component';
+import { SettingPageComponent } from './setting-page/setting-page.component';
 import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'weighing', component: WeighingPageComponent },
+{ path: 'setting', component: SettingPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [WeighingPageComponent, SettingPageComponent];
+
---
+++ frontend/src/app/weighing-page/weighing-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weighing-page',
+  templateUrl: './weighing-page.component.html',
+  styleUrls: ['./weighing-page.component.css']
+})
+export class WeighingPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
