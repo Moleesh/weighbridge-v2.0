7802a3d397cb05ec70701e133f053068b778fdce
Moleesh <moleesh1@gmail.com>
2019-02-17 22:02:56 +0530

    browser

--- backend/src/main/java/com/babulens/weighbridge/WeighbridgeApplication.java
+++ backend/src/main/java/com/babulens/weighbridge/WeighbridgeApplication.java
@@ -1,13 +1,43 @@
 package com.babulens.weighbridge;
 
+import org.panda_lang.pandomium.Pandomium;
+import org.panda_lang.pandomium.settings.PandomiumSettings;
+import org.panda_lang.pandomium.wrapper.PandomiumBrowser;
+import org.panda_lang.pandomium.wrapper.PandomiumClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 @SpringBootApplication
 public class WeighbridgeApplication {
     
     public static void main(String[] args) {
+        Pandomium pandomium = new Pandomium(PandomiumSettings.getDefaultSettings());
+        pandomium.initialize();
+
+        PandomiumClient client = pandomium.createClient();
+
         SpringApplication.run(WeighbridgeApplication.class, args);
+        PandomiumBrowser browser = client.loadURL("localhost:8080");
+
+        JFrame frame = new JFrame();
+        frame.getContentPane().add(browser.toAWTComponent(), BorderLayout.CENTER);
+
+        frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                frame.dispose();
+            }
+        });
+
+        frame.setTitle("Pandomium");
+        frame.setSize(1720, 840);
+        frame.setVisible(true);
     }
     
 }
