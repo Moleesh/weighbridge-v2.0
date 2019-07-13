86be60b790aedb704a69d6238da0cc6bf7df3037
moleesh <123456aA>
2019-07-14 04:14:20 +0530

    fixed

--- backend/src/main/java/com/babulens/weighbridge/service/SerialPortService.java
+++ backend/src/main/java/com/babulens/weighbridge/service/SerialPortService.java
@@ -5,5 +5,9 @@ import java.util.List;
 public interface SerialPortService {
     int getWeight();
 
+    void initializeSettings();
+
+    void setCommPort();
+
     List<String> getAllSerialPort();
 }
--- backend/src/main/java/com/babulens/weighbridge/serviceImpl/SerialPortServiceImpl.java
+++ backend/src/main/java/com/babulens/weighbridge/serviceImpl/SerialPortServiceImpl.java
@@ -8,51 +8,62 @@ import com.fazecast.jSerialComm.SerialPortMessageListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @Service
 public class SerialPortServiceImpl implements SerialPortService {
-    private static String indicatorCOMPort = null;
-    private static String indicatorFlowControl = null;
-    private static String indicatorBaudRate = null;
-    private static String indicatorDataBits = null;
-    private static String indicatorParity = null;
-    private static String indicatorStopBits = null;
-    private static String indicatorDelimiter = null;
-    private static int weight = 0;
-    private static SerialPort comPort = null;
+    private String indicatorCOMPort = null;
+    private String indicatorFlowControl = null;
+    private String indicatorBaudRate = null;
+    private String indicatorDataBits = null;
+    private String indicatorParity = null;
+    private String indicatorStopBits = null;
+    private String indicatorDelimiter = null;
+    private int weight = 0;
+    private SerialPort comPort = null;
 
     @Autowired
-    private static SettingsService settingsService;
+    private SettingsService settingsService;
 
-    private static void initializeSettings() {
-        Map<String, String> settings = SerialPortServiceImpl.settingsService.getAllSettings();
-        SerialPortServiceImpl.indicatorCOMPort = settings.get("indicatorCOMPort");
-        SerialPortServiceImpl.indicatorBaudRate = settings.get("indicatorBaudRate");
-        SerialPortServiceImpl.indicatorDataBits = settings.get("indicatorDataBits");
-        SerialPortServiceImpl.indicatorParity = settings.get("indicatorParity");
-        SerialPortServiceImpl.indicatorStopBits = settings.get("indicatorStopBits");
-        SerialPortServiceImpl.indicatorFlowControl = settings.get("indicatorFlowControl");
-        SerialPortServiceImpl.indicatorDelimiter = settings.get("indicatorDelimiter");
+    @PostConstruct
+    public void PostConstruct() {
+        initializeSettings();
+        setCommPort();
     }
 
-    public static void setCommPort() {
-        if (SerialPortServiceImpl.indicatorCOMPort == null) {
-            SerialPortServiceImpl.initializeSettings();
-            System.out.println("here");
+    @Override
+    public void initializeSettings() {
+
+        Map<String, String> settings = this.settingsService.getAllSettings();
+        this.indicatorCOMPort = settings.get("indicatorCOMPort");
+        this.indicatorBaudRate = settings.get("indicatorBaudRate");
+        this.indicatorDataBits = settings.get("indicatorDataBits");
+        this.indicatorParity = settings.get("indicatorParity");
+        this.indicatorStopBits = settings.get("indicatorStopBits");
+        this.indicatorFlowControl = settings.get("indicatorFlowControl");
+        this.indicatorDelimiter = settings.get("indicatorDelimiter");
+    }
+
+    @Override
+    public void setCommPort() {
+
+        if (this.indicatorCOMPort == null) {
+            this.initializeSettings();
         }
         for (SerialPort serialPort : SerialPort.getCommPorts()) {
-            if (serialPort.getSystemPortName().equals(SerialPortServiceImpl.indicatorCOMPort)) {
-                SerialPortServiceImpl.comPort = serialPort;
+            if (serialPort.getSystemPortName().equals(this.indicatorCOMPort)) {
+                this.comPort = serialPort;
                 break;
             }
         }
-        if (SerialPortServiceImpl.comPort != null) {
-            SerialPortServiceImpl.comPort.setComPortParameters(Integer.parseInt(0 + SerialPortServiceImpl.indicatorBaudRate.replaceAll("[^-0-9]", "")), Integer.parseInt(0 + SerialPortServiceImpl.indicatorDataBits.replaceAll("[^-0-9]", "")), Integer.parseInt(0 + SerialPortServiceImpl.indicatorStopBits.replaceAll("[^-0-9]", "")), Integer.parseInt(0 + SerialPortServiceImpl.indicatorParity.replaceAll("[^-0-9]", "")));
-            SerialPortServiceImpl.comPort.openPort();
-            SerialPortServiceImpl.comPort.addDataListener(new SerialPortMessageListener() {
+        if (this.comPort != null) {
+            System.out.println("here" + indicatorCOMPort);
+            this.comPort.setComPortParameters(Integer.parseInt(0 + this.indicatorBaudRate.replaceAll("[^-0-9]", "")), Integer.parseInt(0 + this.indicatorDataBits.replaceAll("[^-0-9]", "")), Integer.parseInt(0 + this.indicatorStopBits.replaceAll("[^-0-9]", "")), Integer.parseInt(0 + this.indicatorParity.replaceAll("[^-0-9]", "")));
+            this.comPort.openPort();
+            this.comPort.addDataListener(new SerialPortMessageListener() {
                 @Override
                 public int getListeningEvents() {
                     return SerialPort.LISTENING_EVENT_DATA_RECEIVED;
@@ -60,7 +71,7 @@ public class SerialPortServiceImpl implements SerialPortService {
 
                 @Override
                 public byte[] getMessageDelimiter() {
-                    return new byte[]{(byte) (Integer.parseInt(0 + SerialPortServiceImpl.indicatorBaudRate.replaceAll("[^-0-9]", "")) % 128)};
+                    return new byte[]{(byte) (Integer.parseInt(0 + indicatorDelimiter.replaceAll("[^-0-9]", "")) % 128)};
                 }
 
                 @Override
@@ -70,7 +81,7 @@ public class SerialPortServiceImpl implements SerialPortService {
 
                 @Override
                 public void serialEvent(SerialPortEvent event) {
-                    SerialPortServiceImpl.weight = Integer.parseInt(0 + new String(event.getReceivedData()).replaceAll("[^-0-9]", ""));
+                    weight = Integer.parseInt(0 + new String(event.getReceivedData()).replaceAll("[^-0-9]", ""));
                 }
             });
         }
@@ -78,7 +89,7 @@ public class SerialPortServiceImpl implements SerialPortService {
 
     @Override
     public int getWeight() {
-        return SerialPortServiceImpl.weight;
+        return this.weight;
     }
 
     @Override
