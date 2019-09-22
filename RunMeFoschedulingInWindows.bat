23ccb2d28e8586886ec15511b46e75dafaad29d9
devjish.a <devjish.a@zohocorp.com>
2019-09-22 21:03:52 +0530

    fixes
    Signed-off-by: devjish.a <devjish.a@zohocorp.com>

--- RunMeFoschedulingInWindows.bat
+++ RunMeFoschedulingInWindows.bat
@@ -3,7 +3,11 @@ net file 1>nul 2>nul && goto :run || powershell -ex unrestricted -Command "Start
 goto :eof
 
 :run
-    echo @echo off > "%programdata%\Microsoft\Windows\Start Menu\Programs\StartUp\weighbridge.bat"
-	echo javaw - java %cd%\weighbridge.jar >> "%programdata%\Microsoft\Windows\Start Menu\Programs\StartUp\weighbridge.bat"
+    echo Set WshShell = CreateObject("WScript.Shell") > "%programdata%\Microsoft\Windows\Start Menu\Programs\StartUp\weighbridge.vbs"
+	echo WshShell.Run chr(34) & "C:\Batch Files\syncfiles.bat" & Chr(34), 0 >> "%programdata%\Microsoft\Windows\Start Menu\Programs\StartUp\weighbridge.vbs"
+	echo Set WshShell = Nothing  >> "%programdata%\Microsoft\Windows\Start Menu\Programs\StartUp\weighbridge.vbs"
+	echo cd %cd% > weighbridge.bat
+    echo javaw -jar weighbridge.jar \>\> sys.out >> weighbridge.bat
+
 EXIT
 
