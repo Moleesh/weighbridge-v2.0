9b2e99a8b1a90553612bf935bbddb0325eefcf7d
Moleesh <moleesh1@gmail.com>
2019-12-31 19:38:49 +0530

    Password
    Signed-off-by: Moleesh <moleesh1@gmail.com>

--- backend/src/main/java/com/babulens/weighbridge/controller/ErrorController.java
+++ backend/src/main/java/com/babulens/weighbridge/controller/ErrorController.java
@@ -1,10 +1,11 @@
 package com.babulens.weighbridge.controller;
 
-import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
-public class MyErrorController implements ErrorController {
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
 	@Override
 	public String getErrorPath() {
 		return "/error";
