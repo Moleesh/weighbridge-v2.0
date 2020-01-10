2be1326869000471abb4928838adc32809c33ee8
Moleesh <moleesh1@gmail.com>
2020-01-11 00:10:32 +0530

    Exception
    Signed-off-by: Moleesh <moleesh1@gmail.com>

---
+++ backend/src/main/java/com/babulens/weighbridge/configuration/SerialPortMessageListenerWithExceptions.java
@@ -0,0 +1,51 @@
+package com.babulens.weighbridge.configuration;
+
+import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListenerWithExceptions;
+import com.fazecast.jSerialComm.SerialPortEvent;
+import com.fazecast.jSerialComm.SerialPortMessageListener;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public final class SerialPortMessageListenerWithExceptions implements SerialPortDataListenerWithExceptions, SerialPortMessageListener {
+
+	private String delimiter;
+	private String lastCharacter;
+	private int weight = -1;
+
+	public SerialPortMessageListenerWithExceptions(String delimiter, String lastCharacter) {
+		this.delimiter = delimiter;
+		this.lastCharacter = lastCharacter;
+	}
+
+	@Override
+	public void catchException(Exception ex) {
+		Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
+	}
+
+	@Override
+	public int getListeningEvents() {
+		return SerialPort.LISTENING_EVENT_DATA_RECEIVED;
+	}
+
+	@Override
+	public byte[] getMessageDelimiter() {
+		return new byte[]{(byte) ' ', (byte) (Integer.parseInt(0 + delimiter.replaceAll("[^-0-9]", "")) % 128)};
+	}
+
+	@Override
+	public boolean delimiterIndicatesEndOfMessage() {
+		return true;
+	}
+
+	@Override
+	public void serialEvent(SerialPortEvent event) {
+		weight =
+				Integer.parseInt(0 + new String(event.getReceivedData()).replaceAll("[^-0-9" + lastCharacter + "]", "").split(lastCharacter)[0]);
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+}
