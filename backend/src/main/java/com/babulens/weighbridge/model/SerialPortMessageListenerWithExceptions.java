69dde336329027e604b9e315a31b9d813a778bfa
Moleesh <moleesh1@gmail.com>
2020-01-10 22:23:10 +0530

    Exception
    Signed-off-by: Moleesh <moleesh1@gmail.com>

---
+++ backend/src/main/java/com/babulens/weighbridge/model/SerialPortMessageListenerWithExceptions.java
@@ -0,0 +1,7 @@
+package com.babulens.weighbridge.model;
+
+import com.fazecast.jSerialComm.SerialPortDataListenerWithExceptions;
+import com.fazecast.jSerialComm.SerialPortMessageListener;
+
+public interface SerialPortMessageListenerWithExceptions extends SerialPortDataListenerWithExceptions, SerialPortMessageListener {
+}
