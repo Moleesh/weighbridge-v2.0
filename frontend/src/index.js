021c0dbd4518283d3a25c0da3c2667802ca3b8fa
Moleesh <moleesh1@gmail.com>
2020-02-24 07:04:24 +0530

    Formating
    Signed-off-by: Moleesh <moleesh1@gmail.com>

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./components/app";
@@ -9,6 +10,5 @@ import "react-bootstrap-typeahead/css/Typeahead-bs4.css";
 
 import "./css/index.css";
 
-ReactDOM.render( < App / >, document.getElementById("root")
-)
+ReactDOM.render(< App/>, document.getElementById("root"))
 
