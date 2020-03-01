7824e815ed3d7254c168953873a55c91467181e3
Moleesh <moleesh1@gmail.com>
2020-03-01 23:40:05 +0530

    fix

--- backend/src/main/java/com/babulens/weighbridge/serviceImpl/TareWeightServiceImpl.java
+++ backend/src/main/java/com/babulens/weighbridge/serviceImpl/TareWeightServiceImpl.java
@@ -4,6 +4,8 @@ import com.babulens.weighbridge.model.entity.TareWeight;
 import com.babulens.weighbridge.repository.TareWeightDAO;
 import com.babulens.weighbridge.service.TareWeightService;
 import com.google.common.collect.Lists;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,21 +21,25 @@ public class TareWeightServiceImpl implements TareWeightService {
 	}
 
 	@Override
+	@Cacheable(cacheNames = "TareWeights")
 	public TareWeight getTareWeightByVehicleNo(String vehicleNo) {
 		return tareWeightDAO.findById(vehicleNo).orElse(null);
 	}
 
 	@Override
+	@Cacheable(cacheNames = "TareWeights")
 	public List<TareWeight> getAllTareWeights() {
 		return Lists.newArrayList(tareWeightDAO.findAll());
 	}
 
 	@Override
+	@CacheEvict(value = "TareWeights", allEntries = true)
 	public TareWeight addUpdateTareWeight(TareWeight tareWeight) {
 		return tareWeightDAO.save(tareWeight);
 	}
 
 	@Override
+	@CacheEvict(value = "TareWeights", allEntries = true)
 	public void deleteTareWeight(String vehicleNo) {
 		tareWeightDAO.deleteById(vehicleNo);
 	}
