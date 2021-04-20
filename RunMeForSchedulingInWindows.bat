9a0296eb0e996589af559ce802d90d97dbf65d27
Moleesh <moleesh1@gmail.com>
2021-04-20 11:12:43 +0530

    RunMeForSchedulingInWindows.bat changes

--- RunMeForSchedulingInWindows.bat
+++ RunMeForSchedulingInWindows.bat
@@ -18,6 +18,23 @@ goto :eof
     echo taskkill /f /im javaw.exe > "%CURRENT_DIR%\WeighbridgeStop.bat"
     echo taskkill /f /im ngrok.exe >> "%CURRENT_DIR%\WeighbridgeStop.bat"
 
+    echo %CURRENT_DIVE% > "%CURRENT_DIR%\ServerStarting.bat"
+    echo cd "%CURRENT_DIR%" >> "%CURRENT_DIR%\ServerStarting.bat"
+    echo taskkill /f /im ngrok.exe >> "%CURRENT_DIR%\ServerStarting.bat"
+    echo start /b ngrok start -config ngrok.yml babulens >> "%CURRENT_DIR%\ServerStarting.bat"
+
+    echo Set WshShell = CreateObject("WScript.Shell") > "%CURRENT_DIR%\ServerStarting.vbs"
+    echo WshShell.Run chr(34) ^& "%CURRENT_DIR%\ServerStarting.bat" ^& Chr(34), 0 >> "%CURRENT_DIR%\ServerStarting.vbs"
+    echo Set WshShell = Nothing  >> "%CURRENT_DIR%\ServerStarting.vbs"
+
+    echo Set oWS = WScript.CreateObject("WScript.Shell") > "%CURRENT_DIR%\temp.vbs"
+    echo sLinkFile = "%UserProfile%\Desktop\ServerStarting.lnk" >> "%CURRENT_DIR%\temp.vbs"
+    echo Set oLink = oWS.CreateShortcut(sLinkFile) >> "%CURRENT_DIR%\temp.vbs"
+    echo oLink.TargetPath = "%CURRENT_DIR%\ServerStarting.vbs" >> "%CURRENT_DIR%\temp.vbs"
+    echo oLink.IconLocation = "%CURRENT_DIR%\babulensLogo.bmp" >> "%CURRENT_DIR%\temp.vbs"
+    echo oLink.Save >> "%CURRENT_DIR%\temp.vbs"
+    cscript /nologo "%CURRENT_DIR%\temp.vbs"
+
     echo Set WshShell = CreateObject("WScript.Shell") > "%CURRENT_DIR%\Weighbridge.vbs"
     echo WshShell.Run chr(34) ^& "%CURRENT_DIR%\WeighbridgeStart.bat" ^& Chr(34), 0 >> "%CURRENT_DIR%\Weighbridge.vbs"
     echo Set WshShell = Nothing  >> "%CURRENT_DIR%\Weighbridge.vbs"
@@ -30,7 +47,6 @@ goto :eof
     echo oLink.WorkingDirectory = "%CURRENT_DIR%" >> "%CURRENT_DIR%\temp.vbs"
     echo oLink.IconLocation = "%CURRENT_DIR%\babulensLogo.bmp" >> "%CURRENT_DIR%\temp.vbs"
     echo oLink.Save >> "%CURRENT_DIR%\temp.vbs"
-
     cscript /nologo "%CURRENT_DIR%\temp.vbs"
 
     echo Set oWS = WScript.CreateObject("WScript.Shell") > "%CURRENT_DIR%\temp.vbs"
